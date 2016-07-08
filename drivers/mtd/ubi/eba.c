86a02d9df52d4877310e62e2c3c95adc5a54aba7
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-07-08 15:04:52 +0200

    UBI: get rid of ->lpos
    This information can be easily retrieved (index in the consolidated LEB
    table).
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/ubi/eba.c
+++ drivers/mtd/ubi/eba.c
@@ -839,7 +839,7 @@ int ubi_eba_write_leb(struct ubi_device *ubi, struct ubi_volume *vol, int lnum,
 		if (full) {
 			int ret;
 
-			ret = ubi_conso_add_full_leb(ubi, vol_id, lnum, 0);
+			ret = ubi_conso_add_full_leb(ubi, vol_id, lnum);
 			if (ret)
 				ubi_warn(ubi,
 					 "failed to add LEB %d:%d to the full LEB list",
@@ -905,7 +905,7 @@ retry:
 	up_read(&ubi->fm_eba_sem);
 
 	if (full) {
-		err = ubi_conso_add_full_leb(ubi, vol_id, lnum, 0);
+		err = ubi_conso_add_full_leb(ubi, vol_id, lnum);
 		if (err)
 			ubi_warn(ubi,
 				 "failed to add LEB %d:%d to the full LEB list",
@@ -1039,7 +1039,7 @@ retry:
 	vol->used_ebs = used_ebs; //XXX
 	up_read(&ubi->fm_eba_sem);
 
-	err = ubi_conso_add_full_leb(ubi, vol_id, lnum, 0);
+	err = ubi_conso_add_full_leb(ubi, vol_id, lnum);
 	if (err)
 		ubi_warn(ubi, "failed to add LEB %d:%d to the full LEB list",
 			 vol_id, lnum);
@@ -1173,7 +1173,7 @@ retry:
 	if (full) {
 		int ret;
 
-		ret = ubi_conso_add_full_leb(ubi, vol_id, lnum, 0);
+		ret = ubi_conso_add_full_leb(ubi, vol_id, lnum);
 		if (ret)
 			ubi_warn(ubi,
 				"failed to add LEB %d:%d to the full LEB list",
@@ -1841,8 +1841,7 @@ int ubi_eba_init(struct ubi_device *ubi, struct ubi_attach_info *ai)
 				if (aeb->full)
 					ubi_conso_add_full_leb(ubi,
 							       vol->vol_id,
-							       aeb->desc.lnum,
-							       aeb->peb_pos);
+							       aeb->desc.lnum);
 			}
 		}
 	}
